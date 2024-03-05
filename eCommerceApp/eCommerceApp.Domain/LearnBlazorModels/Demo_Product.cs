535b13e638e92b7d5cb9dc914d22954d3a66f968
Karolina <karlazar11@gmail.com>
2024-03-05 13:21:42 +0100

    added some blazor functionality.

--- eCommerceApp/eCommerceApp.Domain/LearnBlazorModels/Demo_Product.cs
+++ eCommerceApp/eCommerceApp.Domain/LearnBlazorModels/Demo_Product.cs
@@ -9,7 +9,9 @@ namespace eCommerceApp.Domain.LearnBlazorModels
     public class Demo_Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public bool IsActive { get; set; }
+        public double Price { get; set; }
+        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
